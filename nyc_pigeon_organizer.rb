b3a2a732642dc511a6af286a086665b8b8dc693d
Finn Thye <piqueen@gmail.com>
2019-08-11 20:49:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,22 +19,5 @@ def nyc_pigeon_organizer(data)
   end
   
   pigeons
-  #binding.pry
 end
-
-=begin
-pigeons => {:color=>
-  {:purple=>["Theo", "Peter Jr.", "Lucky"],
-   :grey=>["Theo", "Peter Jr.", "Ms. K"],
-   :white=>["Queenie", "Andrew", "Ms. K", "Alex"],
-   :brown=>["Queenie", "Alex"]},
- :gender=>
-  {:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-   :female=>["Queenie", "Ms. K"]},
- :lives=>
-  {"Subway"=>["Theo", "Queenie"],
-   "Central Park"=>["Alex", "Ms. K", "Lucky"],
-   "Library"=>["Peter Jr."],
-   "City Hall"=>["Andrew"]}}
-=end
    
