0ece4602ea47c64b7f6fb4b5962b8e7c7006faed
Finn Thye <piqueen@gmail.com>
2019-08-11 19:26:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  
+  #iterate over each pigeon, creating a key pointing to a hash value
+  data.each do |key, value|
+    pigeons[key] = value
+  end
+  
+  pigeons
+  binding.pry
 end
